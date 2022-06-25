65a4f81cad34f814351baa0411f1c6de0130dd1e
Athus Cavalini <athuscavalini@MacBook-Pro-de-Athus.local>
2022-06-25 18:28:38 -0300

    multiple_subs_update

--- multiple_subs.py
+++ multiple_subs.py
@@ -21,16 +21,17 @@ def new_sub(cid = 'sub'):
     client.on_message = on_message
     client.loop_forever()
 
-with open('subs_data.csv', 'a+', newline='') as write_obj:
-    csv_writer = writer(write_obj)
-    n = 1
-    while True:
-        print(f"Iniciada thread {n}")
-        threading.Thread(target=new_sub, args=(str(n),)).start()
-        n += 1
-        time.sleep(5)
+
+n = 0
+while True:
+    with open('subs_data.csv', 'a+', newline='') as write_obj:
+        csv_writer = writer(write_obj)
         cpu = psutil.cpu_percent(5)
         ram = psutil.virtual_memory()[2]
         print('CPU usage: ', cpu)
         print('RAM usage:', ram)
         csv_writer.writerow([n, cpu, ram])
+
+    n += 1
+    print(f"Iniciada thread {n}")
+    threading.Thread(target=new_sub, args=(str(n),)).start()
